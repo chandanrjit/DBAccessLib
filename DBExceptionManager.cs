eead8319d8dc58cb6a7afb54269d155b100dae37
Chandan Kumar <chandanroy_07@hotmail.com>
2021-03-04 21:36:38 +1300

    Exception Handling

---
+++ DBExceptionManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBHelperpoc
+{
+    class DBExceptionManager
+    {
+        #region Instance Property
+        private static DBExceptionManager _Instance;
+
+        public static DBExceptionManager Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new DBExceptionManager();
+                }
+
+                return _Instance;
+            }
+            set { _Instance = value; }
+        }
+        #endregion
+
+        /// <summary>
+        /// Get/Set Last Exception Object Created
+        /// </summary>
+        public Exception LastException { get; set; }
+
+        #region Publish Methods
+        public virtual void Publish(Exception ex)
+        {
+            LastException = ex;
+
+            // TODO: Implement an exception publisher here
+            System.Diagnostics.Debug.WriteLine(ex.ToString());
+        }
+
+        public virtual void Publish(Exception ex, SqlCommand cmd)
+        {
+            Publish(ex, cmd, null);
+        }
+
+        public virtual void Publish(Exception ex, SqlCommand cmd, string exceptionMsg)
+        {
+            LastException = ex;
+
+            if (cmd != null)
+            {
+                LastException = CreateDbException(ex, cmd, null);
+
+                // TODO: Implement an exception publisher here
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+        }
+        #endregion
+
+        #region CreateDbException Method
+        public virtual DBDataException CreateDbException(Exception ex, SqlCommand cmd, string exceptionMsg)
+        {
+            DBDataException exc;
+            exceptionMsg = string.IsNullOrEmpty(exceptionMsg) ? string.Empty : exceptionMsg + " - ";
+
+            exc = new DBDataException(exceptionMsg + ex.Message, ex)
+            {
+                ConnectionString = cmd.Connection.ConnectionString,
+                Database = cmd.Connection.Database,
+                SQL = cmd.CommandText,
+                CommandParameters = cmd.Parameters,
+                WorkstationId = Environment.MachineName
+            };
+
+            return exc;
+        }
+        #endregion
+    }
+}
